257d2619168fd06aa31101b1f471b5be30f291a7
seen <sina.hashemi2a@gmail.com>
2026-02-04 17:29:44 +0300

    Fix image loading - add remote patterns

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,30 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.pixabay.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.supabase.co',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.freemusicarchive.org',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.soundhelix.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
